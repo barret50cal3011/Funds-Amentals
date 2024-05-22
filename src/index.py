f8bb2b0f220631c57658853806ee043fce7d4eb7
DanielBlaDi <dablancod@unal.edu.co>
2024-05-22 00:06:30 -0500

    Repaired an error in index.py

--- src/index.py
+++ src/index.py
@@ -1,7 +1,4 @@
 
 
 if __name__ == '__main__':
-    hi = {"ArabOilCompany": "Oil", "Doors": "Software", "Edison": "Electricity"
-                             , "GamePause": "VideoGames", "mvidia": "PC Components", 
-                             "pear": "Smartphones" }
-    print(hi)
+    pass
