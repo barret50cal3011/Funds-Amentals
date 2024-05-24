f5432f424f512bfc0b639595fcc8023ef2a9f1ac
jdym3119 <jyopasam@unal.edu.co>
2024-05-24 12:34:19 -0500

    password successfully encrypted

--- src/login.py
+++ src/login.py
@@ -10,7 +10,6 @@ pwd: str = password.encode('utf-8')
 hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
 user = User(name, hashed)
 
-<<<<<<< HEAD
 while True:
     answer: str = input("Would you like to change something (y/n)? ")
     if answer.lower() == "y":
@@ -46,31 +45,3 @@ while True:
         break
 
 print("Goodbye!")
-=======
-name:str = input("What is your name? ")
-age:str = input("How old are you? ")
-password:str = input("What is your password? ")
-user = User(name, age, password)
-while True:
-  answer:str=input("Woul you like to change something or view data(y/n)? ")
-  if answer.lower() == "y":
-    while True:
-      print("1. Name\n2. Age\n3. Password\n4. View data\n5. Exit")
-      secondAnswer:str=input("What's you're choice? ")
-      match secondAnswer:
-        case "1":
-          name:str = input("What is your name? ")
-          user.set_name(name)
-        case "2":
-          age:str = input("How old are you? ")
-          user.set_age(age)
-        case "3":
-          password:str = input("What is your password (y/n)? ")
-          user.sert_password(password)
-        case "4":
-          print(user)
-        case "5":
-         break
-  elif answer.lower()=="n":
-    break
->>>>>>> b4c195fc3bbe0f640a68509c6e83becfb18c6216
--- src/user.py
+++ src/user.py
@@ -1,16 +1,12 @@
 class User:
-<<<<<<< HEAD
   def __init__(self, name:str, password:bytes):
-=======
-  def __init__(self, name:str, age:str, password:str):
->>>>>>> b4c195fc3bbe0f640a68509c6e83becfb18c6216
     self.__name=name
     self.__password=password
   def get_name(self):
     return self.__name
   def set_name(self, name:str):
     self.__name=name
-  def set_age(self, age:str):
+  def set_age(self, age:int):
     self.__age=age
   def set_password(self, password:str):
     self.__password=password
