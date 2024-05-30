a585cbe5e72c7e92a1e06c0e9b8e7980abad7193
jdym3119 <jyopasam@unal.edu.co>
2024-05-30 13:38:27 -0500

    upload world

--- src/world.py
+++ src/world.py
@@ -3,4 +3,7 @@ import stocks
 import Player
 
 class World:
-    pass
+    def __init__(self,events,player):
+        self.events = events.Events()
+        self.stocks = stocks.Stocks()
+        self.player = Player.Player()
