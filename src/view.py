121fa6e264ee717131cd74ae1f8577728bdb3e3b
B4RRET <barret50cal3011@gmail.com>
2024-06-08 17:57:28 -0400

    changed the commands buy stocks and sell setocks to buy stock and sell stock and fixed help command

--- src/view.py
+++ src/view.py
@@ -12,8 +12,8 @@ class View:
         """
         help [name of a command]
         read_news
-        buy_stocks <name of the company> <amount of stocks>
-        sell_stocks <name of the company> <amount of stocks>
+        buy_stock <name of the company> <amount of stocks>
+        sell_stock <name of the company> <amount of stocks>
         see_portfolio
         next_week
         exit
@@ -23,44 +23,44 @@ class View:
 
     COMMANDS = {
         "help": 
-        textwrap.dedent("""
+        """
         help [command]. 
         writes a list of commands if no command is given.
         If a command is given, it prints a description of the command that was given(just like this one).
         When using this command <> means that it is obligatory to add the information for the funtionality of the command and [] means that it is optional.
-        """),
-        "read_news [News tittle]": 
-        textwrap.dedent("""
+        """,
+        "read_news": 
+        """
         read_news
         prints a list of the events that are ocurring at that week.
         If a tittle is added the command prints the news article of the news that was selencted.
-        """),
-        "buy_stocks": 
-        textwrap.dedent("""
+        """,
+        "buy_stock": 
+        """
         buy_stock <company name> <stock amount>
         If the money is abailable, buys the amount of stocks of the company specified.
-        """),
-        "sell_stocks": 
-        textwrap.dedent("""
+        """,
+        "sell_stock": 
+        """
         sell_stock <company name> <amount>
         If the amount of stocks are abailable, sells the stock to the current price.
-        """),
+        """,
         "see_portfolio": 
-        textwrap.dedent("""
+        """
         see_portfolio
         prints the information of the player's portfolio.
-        """),
+        """,
         "next_week": 
-        textwrap.dedent("""
+        """
         next_week
         advance the game to the next week.
-        """),
+        """,
         "exit": 
-        textwrap.dedent("""
+        """
         exit
         Exits the game.
         Save is currentlly not implemented so progres on the game will be lost.
-        """)
+        """
     }
 
 
@@ -72,15 +72,16 @@ class View:
         command = input()
         if not command.split(" ")[0] in self.COMMANDS:
             print("Command not recognised.")
-        elif command == "help":
+        elif command.split(" ")[0] == "help":
             if len(command.split(" ")) == 1:
                 print(self.hlp)
             else:
-                command = command.split(" ")[1]
-                if command in self.COMMANDS:
-                    print(self.COMMANDS[command.split(" ")[1]])
+                command_split = command.split(" ")[1]
+                print("command")
+                if command_split in self.COMMANDS:
+                    print(textwrap.dedent(self.COMMANDS[command_split]))
                 else:
-                    print(f"comand {command} not recognized.")
+                    print(f"comand {command_split} not recognized.")
                 #TODO
         elif command == "exit":
             return -1
