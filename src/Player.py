47ec916b1963661f944410a8052e42cc9b4764fc
DanielBlaDi <dablancod@unal.edu.co>
2024-09-12 23:06:50 -0500

    add new method in player that save the stock_price when player buys a stock

--- src/Player.py
+++ src/Player.py
@@ -4,17 +4,32 @@ class Player:
     def __init__(self, i_starting_USD) -> None:
         self.__player_portfolio = {}
         self.__player_portfolio["USD"] = i_starting_USD
-        self.__player_stock_price_buy = {"Edison": 0, "ArabOilCompany": 0, "Doors":  0, "GamePause":  0, "Mvidia":  0, "Pear":  0, 'USWeapons':  0}
+        self.__player_stock_price_buy = {"Edison": {}, "ArabOilCompany": {}, "Doors":  {}, "GamePause":  {}, "Mvidia":  {}, "Pear":  {}, 'USWeapons':  {}}
         
     @property
     def player_balance_money(self):
         return self.__player_portfolio["USD"]
+
+    def register_player_inversion(self, stock: Stock, quantity: float) -> None:
+
+        stock_name = stock.get_company_name()
+        stock_price = stock.get_stock_price()
+
+        # Check if the price of the stock is already in the dictionary of the stock
+        # If not, add it
+        self.__player_stock_price_buy[stock_name].setdefault(stock_price, 0)
+    
+        # Increace the quantity of the stock ammount
+        self.__player_stock_price_buy[stock_name][stock_price] += quantity
+
 
     def buy_stock(self, stock: Stock, quantity: int) -> None:
         if(quantity * stock.get_stock_price() > self.__player_portfolio["USD"]):
             raise ValueError("Not enough USD to buy stock")
         if stock in self.__player_portfolio:
             self.__player_portfolio[stock] += quantity
+            self.__player_portfolio["USD"] -= quantity * stock.get_stock_price()
+
         else:
             self.__player_portfolio[stock] = quantity
         self.__player_portfolio["USD"] -= quantity * stock.get_stock_price()
