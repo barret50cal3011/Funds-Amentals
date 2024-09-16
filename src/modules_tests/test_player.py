60f1457ad48ae3577eb4a51155f62f1848c7c013
DanielBlaDi <dablancod@unal.edu.co>
2024-09-15 23:16:04 -0500

    Create new methods and fuctions that attempt to say to the player how many money spend and earn for each sell and buy of stocks

---
+++ src/modules_tests/test_player.py
@@ -0,0 +1,10 @@
+from src.Player import Player
+from src.stocks_m.stock import Stock
+
+if __name__ == "__main__":
+    player = Player(1000)
+    stock = Stock("Edison", 100)
+    print(player.player_money)
+    print(player.get_portfolio())
+    print(player.buy_stock(stock, 10))
+    print(player.get_stock_amount("Edison"))
