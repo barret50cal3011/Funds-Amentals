080dd7eb82a03c44a7b534a77a9b8c7991f91db3
jdym3119 <jyopasam@unal.edu.co>
2024-09-25 11:37:15 -0500

    news corretion and read_news modified

--- src/news.py
+++ src/news.py
@@ -11,7 +11,7 @@ class News:
         Gets the headlines of all available news based on current events. 
         """
         headlines = []
-        for storer in self.event_storers.values():
+        for storer in self.event_storers.keys():
             active_events = storer.get_active_sons()
             for event in active_events:
                 headlines.append(f"Breaking News: {event.get_event_name()}!")
@@ -21,7 +21,7 @@ class News:
         """
         Gets the news article for a particular event.
         """
-        for storer in self.event_storers.values():
+        for storer in self.event_storers.keys():
             for event in storer.get_active_sons():
                 if event.get_event_name() == event_name:
                     return f"{event.get_event_name()}: {event.description}"
